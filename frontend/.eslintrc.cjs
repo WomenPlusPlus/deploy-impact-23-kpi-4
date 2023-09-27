85d8fe81c2a345e9bcafa4e6fded859713207760
Monica Chmilevski <monica.chmilevski@gmail.com>
2023-09-27 12:04:27 +0200

    set up frontend skeleton project

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,49 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        es2020: true,
+        "jest/globals": true
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react/jsx-runtime',
+        'plugin:react-hooks/recommended',
+        'plugin:@typescript-eslint/recommended'
+    ],
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+    settings: { react: { version: '18.2' } },
+    plugins: ['react-refresh', 'jest'],
+    rules: {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ],
+        "eqeqeq": "error",
+        "no-trailing-spaces": "error",
+        "object-curly-spacing": [
+            "error", "always"
+        ],
+        "arrow-spacing": [
+            "error", { "before": true, "after": true }
+        ],
+        "no-console": 0,
+        "react/prop-types": 0,
+        "react/react-in-jsx-scope": "off",
+        "no-unused-vars": 0
+    },
+}
