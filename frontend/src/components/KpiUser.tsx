48db13f4f821c1bf4e92a3fb5146262f47dbab12
nurp <nurpinar@gmail.com>
2023-10-12 12:59:26 +0200

    add redirect url for after login is successfull

--- frontend/src/components/KpiUser.tsx
+++ frontend/src/components/KpiUser.tsx
@@ -8,6 +8,18 @@ import {
   ThemeSupa,
 } from '@supabase/auth-ui-shared'
 
+/* code from: https://supabase.com/docs/guides/auth/concepts/redirect-urls */
+const getURL = () => {
+  let url =
+    process.env.REACT_APP_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    'http://localhost:3000/'
+  // Make sure to include `https://` when not localhost.
+  url = url.includes('http') ? url : `https://${url}`
+  // Make sure to include a trailing `/`.
+  url = url.charAt(url.length - 1) === '/' ? url : `${url}/`
+  return url
+}
+
 const KpiUser: React.FC = () => {
   const [user, setUser] = useState<User | null>()
 
@@ -45,6 +57,7 @@ const KpiUser: React.FC = () => {
         access_type: 'offline',
         prompt: 'consent',
       }}
+      redirectTo={getURL() + 'login'}
       onlyThirdPartyProviders
     />
   )
